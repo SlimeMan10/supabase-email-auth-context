6413cff58c4aa2530985f4f02db1f8c752afa8fe
jherna3 <jherna3@uw.edu>
2025-02-28 18:42:20 -0800

    p

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,12 @@ import "./index.css";
 
 import { router } from "./router.jsx";
 import { RouterProvider } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <>
       <h1 className="text-center text-3xl pt-4">Supabase Auth & Context</h1>
-      <AuthContextProvider>
-        <RouterProvider router={router} />
-      </AuthContextProvider>
+      <RouterProvider router={router} />
     </>
   </StrictMode>
 );
--- src/router.jsx
+++ src/router.jsx
@@ -2,19 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Signup from "./components/Signup";
 import Signin from "./components/Signin";
-import Dashboard from "./routes/Dashboard";
-import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./components/Dashboard";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/signup", element: <Signup /> },
   { path: "/signin", element: <Signin /> },
-  {
-    path: "/dashboard",
-    element: (
-      <PrivateRoute>
-        <Dashboard />
-      </PrivateRoute>
-    ),
-  },
+  { path: "/", element: <Dashboard />},
 ]);
